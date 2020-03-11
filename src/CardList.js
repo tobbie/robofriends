94cd072dd9a66144e95a50fafd6fe21b610e1652
Oluwatobi Akinseye <tobi.akinseye@gmail.com>
2020-03-11 14:33:11 +0100

    added CardList

---
+++ src/CardList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from './Card'
+
+
+
+const CardList = ({robots}) => {
+  
+  const cardsArray =   robots.map((user, index) => {
+                            return ( <Card key= {index} name = {user.name} email = {user.email} id = {user.id}/>)
+                        });
+    
+   return(<div>
+            {cardsArray}
+         </div>);
+}
+
+export default CardList;
